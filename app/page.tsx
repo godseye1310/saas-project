b872702cdf9f5509a12bfe078aef8243a82e1ae8
Avinash <avinashkmg1310@gmail.com>
2025-07-09 00:57:48 +0530

    + Home page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,39 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsLists from "@/components/CompanionsLists";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
 
 const Page = () => {
 	return (
-		<div>
-			<h1 className="text-2xl">Welcome to my SaaS App</h1>
-			<Button>Click me</Button>
-		</div>
+		<main>
+			<h1>Popular Companions</h1>
+
+			<section className="home-section">
+				{recentSessions.slice(0, 3).map((companion) => (
+					<CompanionCard
+						key={companion.id}
+						id={companion.id}
+						name={companion.name}
+						topic={companion.topic}
+						subject={companion.subject}
+						duration={companion.duration}
+						color={companion.color}
+						// color={getSubjectColor(companion.subject)}
+						bookmarked={false}
+					/>
+				))}
+				{/* <CompanionCard /> */}
+			</section>
+
+			<section className="home-section">
+				<CompanionsLists
+					title="Recently completed sessions"
+					companions={recentSessions}
+					classNames="w-2/3 max-lg:w-full"
+				/>
+				<CTA />
+			</section>
+		</main>
 	);
 };
 
