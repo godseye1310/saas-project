ff8d53bca7efef4f8bbdf2a2945dad4b2dedb50b
Avinash <avinashkmg1310@gmail.com>
2025-07-09 22:09:00 +0530

    Integration with VAPI for Converstation with Companion

--- lib/actions/companions.actions.ts
+++ lib/actions/companions.actions.ts
@@ -48,3 +48,17 @@ export const getAllCompanions = async ({
 
 	return companions;
 };
+
+// Fetch Single companion
+export const getCompanion = async (id: string) => {
+	const supabase = createSupabaseClient();
+
+	const { data, error } = await supabase
+		.from("companions")
+		.select()
+		.eq("id", id);
+
+	if (error) return console.log(error);
+
+	return data[0];
+};
